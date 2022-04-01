a662c458bfd390377b54ccc3350a7dd62726829e
Cristina Pat <calota.cristina@gmail.com>
2022-04-01 20:28:01 +0100

    added the main method

--- src/thesaurus/Main.java
+++ src/thesaurus/Main.java
@@ -3,6 +3,21 @@ package thesaurus;
 public class Main {
 
 	public static void main(String[] args) {
+		Thesaurus x = new Thesaurus();
+		x.populate();
+		x.print();
+		
+		
+		x.addSynonym("happy", "extatic");
+		x.print();
+		
+		x.addSynonym("policeman", "constable");
+		x.print();
+		
+		x.replaceWord("happy", "A policeman's lot is not a happy one");
+
+		x.crossReference();
+		x.print();
 	}
 
 
